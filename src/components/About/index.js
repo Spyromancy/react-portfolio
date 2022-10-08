dbb8c8d4c6f1fbf6a5c2097ff199272b29fd2224
Josh <josh.haddix598@gmail.com>
2022-10-08 17:38:47 -0500

    set up About page

---
+++ src/components/About/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import avatar from "../../assets/images/avatar.jpg";
+
+function About() {
+  return (
+    <div id="about-me">
+      <div className="avatar">
+        <img src={avatar} alt="my avatar"></img>
+      </div>
+      <div className="about">
+        <h2 className="section-title">About Me</h2>
+        <div>
+          <p>
+            I am a hard working programmer who is looking to expand his work
+            portfolio and help bring your projects to life. I'm familiar with
+            MERN, as well as Python and am constantly looking to add more
+            languages to the list. I have done many projects both solo and in
+            group settings in order to create a finished product that meets
+            client standards. You can find a demonstration of some of the works
+            i'm most proud of below so that you can better understand the
+            quality of works i bring to the table.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default About;
